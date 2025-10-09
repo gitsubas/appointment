aff48fbe0198f53edb64f8f969ce5f58ba66cc5a
Subas Parajuli <subas1988.sp@gmail.com>
2025-10-09 11:40:53 +0545

    Change SMTP configuration to use Gmail service

--- server.js
+++ server.js
@@ -40,9 +40,7 @@ app.post('/api/appointment', async (req, res) => {
   });
 
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    service: 'gmail',
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS
