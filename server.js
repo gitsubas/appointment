a3edea5884165732e615d7b9b5d4829c72cc11eb
Subas Parajuli <subas1988.sp@gmail.com>
2025-10-13 20:46:36 +0545

    Change SMTP port from 587 to 465

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.post('/api/appointment', async (req, res) => {
 
   const transporter = nodemailer.createTransport({
     host: 'smtp.mailgun.org',
-    port: 587,
+    port: 465,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS
